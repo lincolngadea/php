87d64ab02738e93a7938f8122248cead819abed8
Lincoln Gadéa <lincoln@glinc.com.br>
2020-07-10 13:26:17 -0300

    classe datetime

--- Estudos/data/exemplo-04.php
+++ Estudos/data/exemplo-04.php
@@ -2,4 +2,13 @@
 
 $dt = new DateTime();
 
-echo $dt;
+$periodo = new DateInterval("P15D"); //Adicionou mais 15 dias a data atual.
+//para entender o parâmetro, checar documentação
+
+echo $dt->format("d/m/Y H:i:s");
+
+$dt->add($periodo);
+
+echo "<br>";
+
+echo $dt->format("d/m/Y H:i:s");
